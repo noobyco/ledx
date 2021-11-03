1dd415eda80567f5a141aed2aa6ba6ff5b072137
Tom Hudson <mail@tomnomnom.com>
2021-11-04 03:41:11 +0530

    Signed-off-by: Md Moinuddin <moinuddinshekh2@gmail.com>
     Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const mqtt = require("mqtt");
+const express = require("express");
+
+const client  = mqtt.connect('mqtt://broker.hivemq.com')
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("static"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+let name;
+let passwd;
+
+app.post("/", function(req, res){
+    name = req.body.name;
+    passwd = req.body.passwd;
+   
+    res.redirect("/main");
+});
+
+
+let command;
+
+app.post("/main", function(req, res){
+    command = req.body.command;
+    if(command == "on" || command == "ON"){
+        
+        client.publish("iot", "on");
+        res.send("LED STATE : ON");
+        
+          
+
+    }else if(command == "off" || command == "OFF"){
+        
+
+        client.publish("iot", "off");
+        res.send("LED STATE : OFF");
+        
+    }
+});
+
+app.get("/main", function(req, res){
+    if(passwd == "raspian1155"){
+
+        res.sendFile(__dirname + "/main.html");
+    }else{
+        res.write("Wrong password, YOU " + name);
+        res.send();
+    }
+});
+
+//Server listning...
+app.listen(process.env.PORT || 8000);
+
